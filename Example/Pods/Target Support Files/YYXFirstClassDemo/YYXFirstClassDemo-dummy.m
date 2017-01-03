9362001cceaf642df10fead6f950b9621e1d8159
admin <admin@admindeiMac-5.local>
2017-01-03 10:49:50 +0800

    第一个完整的cocoapods管理私有库
    Signed-off-by: admin <admin@admindeiMac-5.local>

---
+++ Example/Pods/Target Support Files/YYXFirstClassDemo/YYXFirstClassDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YYXFirstClassDemo : NSObject
+@end
+@implementation PodsDummy_YYXFirstClassDemo
+@end
---
+++ Example/Pods/Target Support Files/YYXFirstClassDemo/YYXFirstClassDemo-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double YYXFirstClassDemoVersionNumber;
+FOUNDATION_EXPORT const unsigned char YYXFirstClassDemoVersionString[];
+

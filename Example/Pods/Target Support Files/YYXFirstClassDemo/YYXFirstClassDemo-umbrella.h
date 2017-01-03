630be966a561487eb6b2f89b70ec7c8e91ddec14
admin <admin@admindeiMac-5.local>
2017-01-03 16:29:02 +0800

    更新

--- Example/Pods/Target Support Files/YYXFirstClassDemo/YYXFirstClassDemo-umbrella.h
+++ Example/Pods/Target Support Files/YYXFirstClassDemo/YYXFirstClassDemo-umbrella.h
@@ -2,6 +2,8 @@
 #import <UIKit/UIKit.h>
 #endif
 
+#import "UIImage+ImageEffects.h"
+#import "UIImage+YXBlur.h"
 
 FOUNDATION_EXPORT double YYXFirstClassDemoVersionNumber;
 FOUNDATION_EXPORT const unsigned char YYXFirstClassDemoVersionString[];
